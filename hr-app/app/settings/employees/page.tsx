09066b05ca4965bbf1fb909118f9f0ff7785ef3a
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-01-01 22:33:48 +0100

    Add some translations

--- hr-app/app/settings/employees/page.tsx
+++ hr-app/app/settings/employees/page.tsx
@@ -1,8 +1,15 @@
+"use client"
+
+import { useTranslation } from 'react-i18next';
+
+
 const EmployeesSettings = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="grid grid-rows-[10px_1fr_10px] min-h-screen p-1 pb-1 sm:p-1 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <text>Employee settings</text>
+                <text>{t('employee.settings.title')}</text>
             </main>
         </div>
     );
--- hr-app/app/settings/roles/page.tsx
+++ hr-app/app/settings/roles/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { useTranslation } from 'react-i18next';
+
 const RolesSettings: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="grid grid-rows-[10px_1fr_10px] min-h-screen p-1 pb-1 sm:p-1 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <text>Roles settings</text>
+                <text>{t('role.settings.title')}</text>
             </main>
         </div>
     );
