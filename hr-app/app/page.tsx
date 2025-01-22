dc4b939fadf389ac797c640f7bb33db1a48eb06c
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-01-22 16:55:55 +0100

    Add info - not logged

--- hr-app/app/page.tsx
+++ hr-app/app/page.tsx
@@ -25,7 +25,13 @@ const Home: React.FC = () => {
                         <Items />
                     </>
                 ) : (
-                    <p>{t('common.message.youAreNotLogged')}</p>
+                    <div className="grid grid-rows-[10px_1fr_10px] justify-items-center min-h-screen p-1 pb-1 sm:p-1 font-[family-name:var(--font-geist-sans)]">
+                        <main className="flex flex-col gap-6 row-start-2 items-center sm:items-center w-full h-full">
+                            <div className="w-full max-w-md p-4 sm:p-6 bg-white shadow-md rounded-lg flex justify-center items-center text-center">
+                                {t('common.message.youAreNotLogged')}
+                            </div>
+                        </main>
+                    </div>
                 )}
             </main>
         </div>
